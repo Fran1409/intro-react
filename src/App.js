74b2e1b655579efcec6f09a7d414852efedf53ea
Fran1409 <fran_andries@hotmail.com>
2021-02-16 12:34:39 +0100

    Add stored todos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import reactDom from "react-dom";
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import ToDoList from './ToDoList';
+import { v4 as uuidv4 } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'toDoApp.todos';
 
 function App() {
   const [todos, setToDos ] = useState([]);
   const toDoNameRef = useRef();
 
+  useEffect(() => {
+    const storedToDos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(storedToDos) setToDos(storedToDos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
   function handleAddToDo(e){
     const name = toDoNameRef.current.value;
     if(name ==='') return;
     setToDos(prevToDos => {
-      return [...prevToDos, {id:1, name:name, complete:false}]
+      return [...prevToDos, {id:uuidv4(), name:name, complete:false}]
     });
     toDoNameRef.current.value = null;
   };

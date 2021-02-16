8406930fa8073362711207a68b1c588cca1cacc5
Fran1409 <fran_andries@hotmail.com>
2021-02-16 12:00:33 +0100

    Add todo that is inserted in to input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import reactDom from "react-dom";
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import ToDoList from './ToDoList';
 
 function App() {
   const [todos, setToDos ] = useState([]);
+  const toDoNameRef = useRef();
+
+  function handleAddToDo(e){
+    const name = toDoNameRef.current.value;
+    if(name ==='') return;
+    setToDos(prevToDos => {
+      return [...prevToDos, {id:1, name:name, complete:false}]
+    });
+    toDoNameRef.current.value = null;
+  };
+
   return (
     <>
       <h1>My TO DO App</h1>
-      <input type="text" />
-      <button>Add todo</button>
+      <input ref={toDoNameRef} type="text" />
+      <button onClick={handleAddToDo}>Add todo</button>
       <ToDoList todoList={todos} />
     </>
   );
--- src/ToDo.js
+++ src/ToDo.js
@@ -3,7 +3,6 @@ import React from 'react'
 export default function ToDo( {todo}) {
     return (
         <div>
-            <h3>TODOS</h3>
             <input type="checkbox" checked="{todo.complete}"/>
             <label>
                 {todo.name}
